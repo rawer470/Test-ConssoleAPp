b887f0101ec1f7b9cd9237cf9e4b8a96fee15c02
rawer470 <75203120+rawer470@users.noreply.github.com>
2022-12-04 17:04:07 +0300

    Хорошее и доброе

--- Test ConssoleAPp/Program.cs
+++ Test ConssoleAPp/Program.cs
@@ -10,6 +10,10 @@ namespace Test_ConssoleAPp
     {
         static void Main(string[] args)
         {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a+b);
+            Console.ReadLine();
         }
     }
 }
